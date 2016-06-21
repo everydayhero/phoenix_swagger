be5f523c200d81b2f05e3cb603cebf17d6f22c93
Mike Buhot <michael.buhot@everydayhero.com.au>
2016-06-21 16:24:11 +1000

    Add support for links, relationships and top level json-api responses
    This is only for items with a single resource document for the data.

--- test/definition_test.exs
+++ test/definition_test.exs
@@ -21,17 +21,19 @@ defmodule PhoenixSwagger.JsonApiTest do
         country :string, "Country"
         birthday :string, "Birthday in YYYY-MM-DD format"
       end
+      link :self, "The link to this user resource"
+      relationship :posts
     end
   end
 
   test "produces expected paginated users schema" do
     users_schema = swagger_definitions["Users"]
     assert users_schema == %{
-      "description" => "A page of [User](#user) results",
+      "description" => "A page of [UserResource](#userresource) results",
       "properties" => %{
         "data" => %{
-          "description" => "Content with [User](#user) objects",
-          "items" => %{"$ref" => "#/definitions/User"},
+          "description" => "Content with [UserResource](#userresource) objects",
+          "items" => %{"$ref" => "#/definitions/UserResource"},
           "type" => "array"
         },
         "links" => %{
@@ -60,12 +62,44 @@ defmodule PhoenixSwagger.JsonApiTest do
     }
   end
 
-  test "produces expected user resource schema" do
-    users_schema = swagger_definitions["User"]
-    assert users_schema == %{
-      "description" => "A user that may have one or more supporter pages.",
+  test "produces expected user top level schema" do
+    user_schema = swagger_definitions["User"]
+    assert user_schema == %{
+      "description" => "A JSON-API document with a single [UserResource](#userresource) resource",
       "properties" => %{
+        "data" => %{
+          "$ref" => "#/definitions/UserResource"
+        },
+        "links" => %{
+          "properties" => %{
+            "self" => %{
+              "description" => "the link that generated the current response document.",
+              "type" => "string"
+            }
+          },
+          "required" => ["self"],
+          "type" => "object"
+        },
+        "included" => %{
+          "description" => "Included resources",
+          "type" => "array"
+        }
+      },
+      "required" => ["links", "data"],
+      "type" => "object"
+    }
+  end
+
+  test "produces expected user resource schema" do
+    user_resource_schema = swagger_definitions["UserResource"]
+    assert user_resource_schema == %{
+      "description" => "A user that may have one or more supporter pages.",
+      "type" => "object",
+      "properties" => %{
+        "id" => %{"description" => "The JSON-API resource ID", "type" => "string"},
+        "type" => %{"description" => "The JSON-API resource type", "type" => "string"},
         "attributes" => %{
+          "type" => "object",
           "properties" => %{
             "birthday" => %{ "description" => "Birthday in YYYY-MM-DD format", "type" => "string"},
             "country" => %{"description" => "Country", "type" => "string"},
@@ -80,11 +114,39 @@ defmodule PhoenixSwagger.JsonApiTest do
             "user_created_at" => %{"description" => "First created timestamp UTC", "type" => "string"},
             "user_updated_at" => %{"description" => "Last update timestamp UTC", "type" => "string", "format" => "ISO-8601"}
           },
-          "type" => "object",
           "required" => ["email"]
+        },
+        "links" => %{
+          "type" => "object",
+          "properties" => %{
+            "self" => %{"description" => "The link to this user resource", "type" => "string"}
+          }
+        },
+        "relationships" => %{
+          "type" => "object",
+          "properties" => %{
+            "posts" => %{
+              "type" => "object",
+              "properties" => %{
+                "data" => %{
+                  "type" => "object",
+                  "properties" => %{
+                    "id" => %{"description" => "Related posts resource id", "type" => "string"},
+                    "type" => %{"description" => "Type of related posts resource", "type" => "string"}
+                  }
+                },
+                "links" => %{
+                  "type" => "object",
+                  "properties" => %{
+                    "related" => %{"description" => "Related posts link", "type" => "string"},
+                    "self" => %{"description" => "Relationship link for posts", "type" => "string"}
+                  }
+                }
+              }
+            }
+          }
         }
-      },
-      "type" => "object"
+      }
     }
   end
 end
