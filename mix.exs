0b05f4e9148f99b808184de4142daa444767b7a2
Mike Buhot <michael.buhot@everydayhero.com.au>
2016-08-30 19:23:03 +1000

    Add Inch-CI support to promote good docs

--- mix.exs
+++ mix.exs
@@ -45,6 +45,7 @@ defmodule PhoenixSwagger.Mixfile do
     [
       {:credo, "~> 0.3", only: [:dev, :test]},
       {:ex_doc, ">= 0.13.0", only: :dev},
+      {:inch_ex, only: :docs},
       {:poison, "~> 2.0"}
     ]
   end
