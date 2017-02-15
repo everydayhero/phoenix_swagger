a02fad18e29544b23b620a6184de16e20e41f6ca
tmaszk <tom.maszk@blackbaud.com>
2017-02-15 11:44:04 -0500

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -51,7 +51,7 @@ defmodule PhoenixSwagger.Mixfile do
       {:credo, "~> 0.3", only: [:dev, :test]},
       {:ex_doc, ">= 0.13.0", only: :dev},
       {:inch_ex, only: :docs},
-      {:poison, "~> 2.0"}
+      {:poison, "~> 3.0"}
     ]
   end
 end
